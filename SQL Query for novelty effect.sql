5015760a87416b4c21ce1997eae3910578887ad3
LizToDataWiz <149129200+LizToDataWiz@users.noreply.github.com>
2023-11-09 15:40:12 -0500

    Add files via upload

---
+++ SQL Query for novelty effect.sql
@@ -0,0 +1,60 @@
+-- Novelty Effect
+-- Inspect the difference in the key metrics between the groups over time.
+
+
+with cte_A as (
+select
+  g.uid as users_id,
+  g.join_dt as join_date,
+	case when g.group = 'A' then 'Control'
+  	else 'Treatment' end as test_group,
+  coalesce(sum(a.spent),0) as total_spent,
+  case when a.spent is NOT NULL then 1
+  		when a.spent is NULL then 0
+      end as spent
+from groups g
+left join activity a on g.uid = a.uid
+--where g.group = 'A'
+group by 1,2,3,5
+order by total_spent desc
+),
+
+control_metrics as (
+select
+  join_date,
+  test_group,
+  round(avg(total_spent),2) as avg_spent,
+  round(avg(spent),4) as conversion_rate
+from cte_A
+where test_group = 'Control'
+group by 1,2
+),
+treatment_metrics as (
+select
+  join_date,
+  test_group,
+  round(avg(total_spent),2) as avg_spent,
+  round(avg(spent),4) as conversion_rate
+from cte_A
+where test_group = 'Treatment'
+group by 1,2
+)
+
+select
+	cm.join_date,
+  --tm.avg_spent as t_avg_spent,
+  --cm.avg_spent as c_avg_spent,
+  tm.avg_spent - cm.avg_spent as diff_avg_spent,
+  --tm.conversion_rate as t_conversion_rate,
+  --cm.conversion_rate as c_conversion_rate,
+  tm.conversion_rate - cm.conversion_rate as diff_conversion_rate
+from control_metrics as cm
+join treatment_metrics as tm on cm.join_date = tm.join_date
+
+
+
+
+
+
+
+
